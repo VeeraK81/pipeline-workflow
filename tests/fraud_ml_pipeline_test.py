d586d49fc9143d5e2ca65d268ec26563e5873b58
veera <veeramanicadas.kannan@gmail.com>
2024-11-15 08:17:53 +0100

    fraud test

--- tests/fraud_ml_pipeline_test.py
+++ tests/fraud_ml_pipeline_test.py
@@ -2,7 +2,7 @@ import pytest
 from unittest import mock
 from app.train import load_data, preprocess_data, create_pipeline, train_model
 
-s3_url = "s3://mymlflowbuc/transactions/data.csv"
+s3_url = "s3://mymlflowbuc/transactions/fraudTest.csv"
 
 # Test data loading
 def test_load_data():
@@ -26,7 +26,8 @@ def test_create_pipeline():
     assert "classifier" in pipe.named_steps, "Classifier missing in pipeline"
 
 # Test model training (mocking GridSearchCV)
-@mock.patch('app.train.GridSearchCV.fit', return_value=None)
+# @mock.patch('app.train.GridSearchCV.fit', return_value=None)
+@mock.patch('app.fraud_detection_train.GridSearchCV.fit', return_value=None)
 def test_train_model(mock_fit):
     pipe = create_pipeline()
     # X_train, X_test, y_train, y_test = preprocess_data(load_data("https://julie-2-next-resources.s3.eu-west-3.amazonaws.com/full-stack-full-time/linear-regression-ft/californian-housing-market-ft/california_housing_market.csv"))
